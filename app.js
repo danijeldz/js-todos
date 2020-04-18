689b01d2215c771aa2e273f03686d437a28b4392
danijeldz <danijeldzankic@hotmail.com>
2020-04-18 16:18:30 +0200

    created filtering feature on todo list

--- app.js
+++ app.js
@@ -29,3 +29,22 @@ list.addEventListener("click", (e) => {
     e.target.parentElement.remove()
   }
 })
+
+// filter todos with search field
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"))
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"))
+}
+
+const search = document.querySelector(".search input")
+
+search.addEventListener("keyup", (e) => {
+  const term = search.value.trim().toLowerCase()
+  filterTodos(term)
+})
